e59e04f53611b05dc716964490b85feb64af2c86
nagyviktor3 <vnagy604@gmail.com>
2022-12-12 21:28:46 +0100

    Update mobil.js

--- mobil.js
+++ mobil.js
@@ -1,15 +1,23 @@
-function checkScreenWidth() 
-    const screenWidth = window.innerWidth;
-    const div = document.querySelector("#login-container");
-    const div2 = document.querySelector("#signup-link");
-  
-    if (screenWidth < 1000) {
-      div.style.paddingBottom = 1000+ "px";
-      div2.style.border = "4px solid black";
-          }
-      
+// const screenWidth = window.innerWidth;
+// const div = document.querySelector("#login-container");
+// const div2 = document.querySelector("#signup-link");
+// // function checkScreenWidth()
+// // const screenWidtwh = window.innerWidth;
+// // const div = document.querySelector("#login-container");
+// // const div2 = document.querySelector("#signup-link");
 
-console.log("================================")
-  
-  // Call the checkScreenWidth function when the page loads
-  window.addEventListener("load", checkScreenWidth)
+// if (screenWidth < 1000) {
+//   div.style.paddingBottom = "1000" + "px";
+//   div2.style.border = "4px solid black";
+// }
+
+console.log("================================");
+
+// Call the checkScreenWidth function when the page loads
+// window.addEventListener("load", checkScreenWidth);
+window.onload = function() {
+    var browserWidth = document.body.clientWidth;
+    if(browserWidth < 1000) {
+        document.getElementById('signup-link').style.paddingBottom = '1100px';
+    }
+};
